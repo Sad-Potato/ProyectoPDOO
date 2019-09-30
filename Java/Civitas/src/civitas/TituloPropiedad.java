9a294284c2218648459b8f8bcab2e2cb4b4cfeaa
SadPotato <e.sergiofern@go.ugr.es>
2019-09-30 11:17:31 +0200

    Added class TituloPropiedad

---
+++ Java/Civitas/src/civitas/TituloPropiedad.java
@@ -0,0 +1,125 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package civitas;
+
+
+public class TituloPropiedad {
+    private float alquilerBase;
+    private static float factorInteresesHipoteca;
+    private float factorRevalorizacion;
+    private float hipotecaBase;
+    private String nombre;
+    private int numCasas;
+    private int numHoteles;
+    private float precioCompra;
+    private float precioEdificar;
+    
+    void actualizaPropietarioPorConversion(Jugador jugador){
+        
+    }
+    
+    Boolean cancelarHipoteca(Jugador jugador){
+        
+    }
+    
+    int cantidadCasasHoteles(){
+        
+    }
+    
+    Boolean comprar(Jugador jugador){
+        
+    }
+    
+    Boolean construirCasa(Jugador jugador){
+        
+    }
+    
+    Boolean construirHotel(Jugador jugador){
+        
+        
+    }
+    
+    Boolean derruirCasas(int n,Jugador jugador){
+        
+    }
+    
+    private Boolean esEstePropietario(Jugador jugador){
+        
+    }
+    
+    public Boolean getHipotecado(){
+        
+    }
+    
+    float getImporteCancelarHipoteca(){
+        
+    }
+    
+    private float getImporteHipoteca(){
+        
+    }
+    
+    String getNombre(){
+        
+    }
+    
+    int getNumCasas(){
+        
+    }
+    
+    int getNumHoteles(){
+        
+    }
+    
+    private float getPrecioAlquiler(){
+        
+    }
+    
+    float getPrecioCompra(){
+        
+    }
+    
+    float getPrecioEdificar(){
+        
+    }
+    
+    private getPrecioVenta(){
+        
+    }
+    
+    Jugador getPropietario(){
+        
+    }
+    
+    Boolean hipotecar(Jugador jugador){
+        
+    }
+    
+    private Boolean propietarioEncarcelado(){
+        
+    }
+    
+    Boolean tienePropietario(){
+        
+    }
+    
+    TituloPropiedad(String nom,float ab,float fr,float hb,float pc,float pe){
+        
+    }
+    
+    public String toString(){
+        
+    }
+    
+    void tramitarAlquiler(Jugador jugador){
+        
+    }
+    
+    Boolean vender(Jugador jugador){
+        
+    }
+    
+}
