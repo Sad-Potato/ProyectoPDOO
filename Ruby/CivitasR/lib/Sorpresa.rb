c53d7fe36e56b4d52c9ce57b764fe48d3cdfe559
Sad-Potato <e.sergiofern@go.ugr.es>
2019-10-08 20:08:54 +0200

    v2.0.1
    Work in progress, changing class Sorpresa in ruby, right now because of this modification it doesn't work.

--- Ruby/CivitasR/lib/Sorpresa.rb
+++ Ruby/CivitasR/lib/Sorpresa.rb
@@ -1,8 +1,37 @@
+require_relative 'diario'
+
+
 module Civitas 
     class Sorpresa
-        def initialize(e)
-          @nomb=e
+      attr_accessor :nomb
+        def self.new_1(tipo,tablero)
         end
-        attr_accessor :nomb
+
+        def self.new_2(tipo,tablero,valor,texto)
+        end
+
+        def self.new_3(tipo,valor,texto)
+        end
+
+        def self.new_4(tipo,mazo)
+        end
+
+        def init
+            @valor=1
+
+        end
+
+        def jugadorCorrecto(actual,todos)
+          return actual<todos.length
+        end
+
+        def informe(actual,todos)
+          Diario.instance.ocurre_evento("")
+        end
+
+        def aplicarAJugador(actual,todos)
+            
+        end
+        
     end
 end
