5a10d8ea7a984110d8df65e43672e3592731c40c
Sad-Potato <e.sergiofern@go.ugr.es>
2019-11-07 19:04:17 +0100

    Test

---
+++ Java/Civitas/src/juegoTexto/test.java
@@ -0,0 +1,10 @@
+package juegoTexto;
+
+import civitas.OperacionesJuego;
+
+public class test{
+    public static void main(String args[]){
+        OperacionesJuego oper=OperacionesJuego.PASAR_TURNO;
+        System.out.println(oper);
+    }
+};
